7391dc2d02645318069b6c69921e7099996c4f59
Francine Eli Barbosa <feli-bar@c1r9p1.42sp.org.br>
2022-12-06 09:00:46 -0300

    initial commit - creating header file

---
+++ so_long.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: feli-bar <feli-bar@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/30 12:32:51 by feli-bar          #+#    #+#             */
+/*   Updated: 2022/12/06 08:46:24 by feli-bar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONH_H
+# define SO_LONG_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <errno.h>
+# include <string.h>
+# include "get_next_line"
+# include "../minilibx/mlx.h"
+
+typedef struct 
