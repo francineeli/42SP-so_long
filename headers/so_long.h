a347f0e32e8794ab4f9ed731b2c061afc28f032b
Francine Eli Barbosa <feli-bar@c1r9p1.42sp.org.br>
2022-12-06 09:12:26 -0300

    initial commit - images and controls files

--- headers/so_long.h
+++ headers/so_long.h
@@ -6,11 +6,11 @@
 /*   By: feli-bar <feli-bar@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 12:32:51 by feli-bar          #+#    #+#             */
-/*   Updated: 2022/12/06 08:46:24 by feli-bar         ###   ########.fr       */
+/*   Updated: 2022/12/06 09:01:12 by feli-bar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SO_LONH_H
+#ifndef SO_LONG_H
 # define SO_LONG_H
 
 # include <unistd.h>
